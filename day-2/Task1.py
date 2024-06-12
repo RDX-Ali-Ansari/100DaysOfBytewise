37662503d65a721e484c2dd1371c08b56d088f2d
Muhammad Ali Qadri <aliansarimuhammadamjad@gmail.com>
2024-06-12 22:45:17 +0500

    Task1.py

--- day-2/Task1.py
+++ day-2/Task1.py
@@ -1,1 +1,9 @@
+def is_palindrome(word):
+    if word == word[::-1]:
+        return f"{word} is a palindrome."
+    else:
+        return f"{word} is not a palindrome."
 
+# Example usage
+print(is_palindrome("racecar"))
+print(is_palindrome("Python"))
