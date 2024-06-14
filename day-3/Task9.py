37ebd996b4f041544fc4f0d40e6a9ed7a2de8b0c
Muhammad Ali Qadri <aliansarimuhammadamjad@gmail.com>
2024-06-14 23:15:47 +0500

    Task9.py

---
+++ day-3/Task9.py
@@ -0,0 +1,11 @@
+def min_edit_distance(str1, str2):
+    m, n = len(str1), len(str2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0:
+                dp[i][j] = j
+            elif j == 0:
+                dp[i][j] = i
+            elif str1[i - 1
