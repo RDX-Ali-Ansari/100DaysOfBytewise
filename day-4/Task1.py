fec8fd26b8582fa47a463e656b3ec756779ebc3e
Muhammad Ali Qadri <aliansarimuhammadamjad@gmail.com>
2024-06-14 23:07:25 +0500

    Task1.py
    QuickSort Algorithm

--- day-4/Task1.py
+++ day-4/Task1.py
@@ -1,1 +1,13 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
 
+# Test the quicksort function
+array = [3, 6, 8, 10, 1, 2, 1]
+sorted_array = quicksort(array)
+print(sorted_array)
