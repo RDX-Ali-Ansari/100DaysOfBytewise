84fb1978f48ecacb1f4c4f39140396bf709675b3
Muhammad Ali Qadri <aliansarimuhammadamjad@gmail.com>
2024-06-29 12:26:38 +0500

    Exercise4.py

---
+++ day-7/Exercise4.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+print("Mean of each feature:", np.mean(X, axis=0))
+print("Median of each feature:", np.median(X, axis=0))
+print("Standard deviation of each feature:", np.std(X, axis=0))
