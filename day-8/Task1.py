20ac22a500a8fc8435048a0675127259f7097889
Muhammad Ali Qadri <aliansarimuhammadamjad@gmail.com>
2024-07-07 13:23:40 +0500

    Update Task1.py

--- day-8/Task1.py
+++ day-8/Task1.py
@@ -1,16 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Load the Titanic dataset
 titanic = pd.read_csv('titanic.csv')
 
-# Extract the 'Survived' column and convert it to a list of values
-survived_counts = titanic['Survived'].value_counts().sort_index()
+list = titanic['Age'].value_counts().sort_index()
 
-# Create a line plot
-plt.figure(figsize=(8, 6))
-plt.plot(survived_counts)
-plt.xlabel('Survived (0 = No, 1 = Yes)')
+plt.plot(list)
+plt.xlabel('Age')
 plt.ylabel('Count')
-plt.title('Trend of Survival on the Titanic')
+plt.title('Trend of Age on the Titanic')
 plt.show()
