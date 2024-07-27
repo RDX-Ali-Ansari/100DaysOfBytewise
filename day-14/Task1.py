39d6fac00d2e4e04fbb26643d6c6d852775ced1f
Muhammad Ali Qadri <aliansarimuhammadamjad@gmail.com>
2024-07-27 10:40:18 +0500

    Task1.py

--- day-14/Task1.py
+++ day-14/Task1.py
@@ -1,1 +1,30 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import accuracy_score, roc_auc_score
 
+loan_data = pd.read_csv('loan_data.csv')
+
+loan_data = loan_data.dropna()
+
+cat_features = ['loan_grade', 'loan_subgrade', 'home_ownership', 'verification_status', 'purpose']
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat', OneHotEncoder(), cat_features)
+    ])
+X = preprocessor.fit_transform(loan_data)
+y = loan_data['loan_status']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+roc_auc = roc_auc_score(y_test, y_pred)
+
+print(f'Accuracy: {accuracy:.2f}')
+print(f'ROC-AUC: {roc_auc:.2f}')
